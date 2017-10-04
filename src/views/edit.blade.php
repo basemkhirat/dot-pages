e092deb63d279ff6697e39d5f1ee611fb132e960
Mohamed Farag <mohamedfarag@dotdev.ae>
2017-10-04 17:44:31 +0200

    update templates

--- src/views/edit.blade.php
+++ src/views/edit.blade.php
@@ -8,32 +8,32 @@
             <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                 <h2>
                     <i class="fa fa-file-text-o"></i>
-                    <?php echo $page->id ? trans("pages::pages.edit") : trans("pages::pages.add_new"); ?>
+                    {{ $page->id ? trans("pages::pages.edit") : trans("pages::pages.add_new") }}
                 </h2>
                 <ol class="breadcrumb">
                     <li>
-                        <a href="<?php echo route("admin"); ?>"><?php echo trans("admin::common.admin") ?></a>
+                        <a href="{{ route("admin") }}">{{ trans("admin::common.admin") }}</a>
                     </li>
                     <li>
-                        <a href="<?php echo route("admin.pages.show"); ?>"><?php echo trans("pages::pages.pages"); ?></a>
+                        <a href="{{ route("admin.pages.show") }}">{{ trans("pages::pages.pages") }}</a>
                     </li>
                     <li class=" active">
                         <strong>
-                            <?php echo $page->id ? trans("pages::pages.edit") : trans("pages::pages.add_new"); ?>
+                            {{ $page->id ? trans("pages::pages.edit") : trans("pages::pages.add_new") }}
                         </strong>
                     </li>
                 </ol>
             </div>
             <div class="col-lg-8 col-md-6 col-sm-6 col-xs-12 text-right">
 
-                <?php if ($page->id) { ?>
-                <a href="<?php echo route("admin.pages.create"); ?>" class="btn btn-primary btn-labeled btn-main"> <span
-                        class="btn-label icon fa fa-plus"></span>  <?php echo trans("pages::pages.add_new") ?></a>
-                <?php } ?>
+                @if ($page->id)
+                <a href="{{ route("admin.pages.create") }}" class="btn btn-primary btn-labeled btn-main"> <span
+                        class="btn-label icon fa fa-plus"></span>  {{ trans("pages::pages.add_new") }}</a>
+                @endif
 
                 <button type="submit" class="btn btn-flat btn-danger btn-main">
                     <i class="fa fa-download" aria-hidden="true"></i>
-                    <?php echo trans("pages::pages.save_page") ?>
+                    {{ trans("pages::pages.save_page") }}
                 </button>
 
             </div>
@@ -43,7 +43,7 @@
 
             @include("admin::partials.messages")
 
-            <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>"/>
+            <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
             <div class="row">
                 <div class="col-md-8">
                     <div class="panel panel-default">
@@ -51,17 +51,17 @@
 
                             <div class="form-group">
                             <textarea name="title" class="form-control input-lg" rows="1" id="post_title"
-                                      placeholder="<?php echo trans("pages::pages.attributes.title") ?>"><?php echo @Request::old("title", $page->title); ?></textarea>
+                                      placeholder="{{ trans("pages::pages.attributes.title") }}">{{ @Request::old("title", $page->title) }}</textarea>
                             </div>
 
                             <div class="form-group">
                             <textarea name="slug" class="form-control input-lg" rows="1" id="post_slug"
-                                      placeholder="<?php echo trans("pages::pages.attributes.slug") ?>"><?php echo @Request::old("slug", $page->slug); ?></textarea>
+                                      placeholder="{{ trans("pages::pages.attributes.slug") }}">{{ @Request::old("slug", $page->slug) }}</textarea>
                             </div>
 
                             <div class="form-group">
                             <textarea name="excerpt" class="form-control" id="post_excerpt"
-                                      placeholder="<?php echo trans("pages::pages.attributes.excerpt") ?>"><?php echo @Request::old("excerpt", $page->excerpt); ?></textarea>
+                                      placeholder="{{ trans("pages::pages.attributes.excerpt") }}">{{ @Request::old("excerpt", $page->excerpt) }}</textarea>
                             </div>
 
                             <div class="form-group">
@@ -76,15 +76,14 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <i class="fa fa-check-square"></i>
-                            <?php echo trans("pages::pages.page_status"); ?>
+                            {{ trans("pages::pages.page_status") }}
                         </div>
                         <div class="panel-body">
                             <div class="form-group switch-row">
                                 <label class="col-sm-9 control-label"
-                                       for="input-status"><?php echo trans("pages::pages.attributes.status") ?></label>
+                                       for="input-status">{{ trans("pages::pages.attributes.status") }}</label>
                                 <div class="col-sm-3">
-                                    <input <?php if (@Request::old("status", $page->status)) { ?> checked="checked"
-                                           <?php } ?> type="checkbox" id="input-status" name="status" value="1"
+                                    <input @if (@Request::old("status", $page->status)) checked="checked" @endif type="checkbox" id="input-status" name="status" value="1"
                                            class="status-switcher switcher-sm">
                                 </div>
                             </div>
@@ -94,19 +93,19 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <i class="fa fa-picture-o"></i>
-                            <?php echo trans("pages::pages.add_image"); ?>
+                            {{ trans("pages::pages.add_image") }}
                         </div>
                         <div class="panel-body form-group">
                             <div class="row post-image-block">
                                 <input type="hidden" name="image_id" class="post-image-id"
-                                       value="<?php echo ($page->image) ? $page->image->id : 0; ?>">
+                                       value="{{ ($page->image) ? $page->image->id : 0 }}">
                                 <a class="change-post-image label" href="javascript:void(0)">
                                     <i class="fa fa-pencil text-navy"></i>
-                                    <?php echo trans("pages::pages.change_image"); ?>
+                                    {{ trans("pages::pages.change_image") }}
                                 </a>
                                 <a class="post-image-preview" href="javascript:void(0)">
                                     <img width="100%" height="130px" class="post-image"
-                                         src="<?php if ($page and @ $page->image) { ?> <?php echo thumbnail(@$page->image->path); ?> <?php } else { ?> <?php echo assets("admin::default/image.png"); ?><?php } ?>">
+                                         src="{{ ($page and @$page->image) ?  thumbnail(@$page->image->path) : assets("admin::default/image.png") }}">
                                 </a>
                             </div>
                         </div>
@@ -115,12 +114,12 @@
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <i class="fa fa-tags"></i>
-                            <?php echo trans("pages::pages.add_tag"); ?>
+                            {{ trans("pages::pages.add_tag") }}
                         </div>
                         <div class="panel-body">
                             <div class="form-group" style="position:relative">
                                 <input type="hidden" name="tags" id="tags_names"
-                                       value="<?php echo join(",", $page_tags); ?>">
+                                       value="{{ join(",", $page_tags) }}">
                                 <ul id="mytags"></ul>
                             </div>
                         </div>
@@ -136,14 +135,14 @@
 @stop
 
 @push("header")
-    <link href="<?php echo assets("admin::tagit")?>/jquery.tagit.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo assets("admin::tagit")?>/tagit.ui-zendesk.css" rel="stylesheet" type="text/css">
+    <link href="{{ assets("admin::tagit") }}/jquery.tagit.css" rel="stylesheet" type="text/css">
+    <link href="{{ assets("admin::tagit") }}/tagit.ui-zendesk.css" rel="stylesheet" type="text/css">
 @endpush
 
 @push("footer")
 
-    <script type="text/javascript" src="<?php echo assets("admin::tagit")?>/tag-it.js"></script>
-    <script type="text/javascript" src="<?php echo assets('admin::ckeditor/ckeditor.js') ?>"></script>
+    <script type="text/javascript" src="{{ assets("admin::tagit") }}/tag-it.js"></script>
+    <script type="text/javascript" src="{{ assets('admin::ckeditor/ckeditor.js') }}"></script>
     <script>
         var baseURL = '{!! URL::to("/".ADMIN) !!}/';
         var postURL = '{!! URL::to(' / details / ') !!}/{!!$page->slug!!}';
@@ -176,7 +175,7 @@
                     }
                 },
                 error: function (media_path) {
-                    alert("<?php echo trans("pages::pages.not_allowed_file") ?>");
+                    alert("{{ trans("pages::pages.not_allowed_file") }}");
                 }
             });
             $("#mytags").tagit({
@@ -188,7 +187,7 @@
                 removeConfirmation: true,
                 tagSource: function (request, response) {
                     $.ajax({
-                        url: "<?php echo route("admin.tags.search"); ?>",
+                        url: "{{ route("admin.tags.search") }}",
                         data: {q: request.term},
                         dataType: "json",
                         success: function (data) {
